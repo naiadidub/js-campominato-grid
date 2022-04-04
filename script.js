20d1a76a77411906f0eaff1b6aed1f0ef1264639
naiadidub <antonio.zaghi1405@gmail.com>
2022-04-04 18:17:30 +0200

    difficoltà aggiunte

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
 //variabile contenete la stringa in cui è scritta la difficoltà
-let difficoltàScelta = document.getElementById('sceltaDifficoltà').value.toString()
 
 
-//funzione play per creare griglia
-let numeroColonne = 100
-let colonnePerRiga = 10
+
+//funzione per creare griglia
+
 function stampagriglia (){
-    let quadratino = document.getElementById('app');
-    quadratino.innerHTML = ''
+    let difficoltàScelta = document.getElementById('sceltaDifficoltà').selectedIndex
+    console.log(difficoltàScelta)
+    let numeroColonne 
+    let colonnePerRiga 
+    if (difficoltàScelta === 0){
+        numeroColonne = 100
+        } else if (difficoltàScelta == 1) {
+            numeroColonne = 81
+        } else {
+            numeroColonne = 49
+        }
+    let quadro = document.getElementById('app');
+    quadro.innerHTML = ''
     let row = document.createElement('div');
     row.setAttribute('class', 'row');
     let cols = creaColonne(numeroColonne);
     row.innerHTML = cols
-    quadratino.append(row)
+    quadro.append(row)
+    cols.addEventListener('click', cambiaColore)
 }
 
 //funzione per creare colonne 
 function creaColonne (numeroColonne){
     let col = ''
-    for (i=0; i < numeroColonne; i++){
+    for (i=1; i <= numeroColonne; i++){
         col += `<div class='col stileCol'>${i}</div>
         `
     }
     return col
 }
-stampagriglia();
+
+//funzione per cambiare colore al quadratino
+function cambiaColore() {
+    cols.classList.add('bluecolor')
+}
