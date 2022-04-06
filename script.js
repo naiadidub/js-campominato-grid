cd66dd249705510659113d6568d3356125655a00
naiadidub <antonio.zaghi1405@gmail.com>
2022-04-06 11:12:14 +0200

    es - fine

--- script.js
+++ script.js
@@ -1,54 +1,80 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+const BOMB_NUMBER = 16;
+const bombs = [];
+let max_attempt;
+let attempts = 0;
 
 function setLevel(event) {
-    //console.log(this);
-    //console.log(event.target);
-    //event.preventDefault();
-    const level = document.getElementById("level").value;
-    console.log("livello selezionato: ", level);
-    let numSquare;
-    switch (level) {
-      case "1":
-      default:
-        numSquare = 100;
-        break;
-      case "2":
-        numSquare = 81;
-        break;
-      case "3":
-        numSquare = 49;
-        break;
+  //console.log(this);
+  //console.log(event.target);
+  //event.preventDefault();
+  const level = document.getElementById("level").value;
+  console.log("livello selezionato: ", level);
+  let numSquare;
+  switch (level) {
+    case "1":
+    default:
+      numSquare = 100;
+      break;
+    case "2":
+      numSquare = 81;
+      break;
+    case "3":
+      numSquare = 49;
+      break;
+  }
+  let squareperSide = Math.sqrt(numSquare);
+  console.log("celle per lato: ", squareperSide);
+  generateBomb(numSquare);
+  generaGriglia(numSquare, squareperSide);
+}
+
+function generateBomb(numSquare) {
+  max_attempt = numSquare - BOMB_NUMBER;
+  while (bombs.length < BOMB_NUMBER) {
+    let bombNUmber = getRandomInt(1, numSquare);
+    if (!bombs.includes(bombNUmber)) {
+      bombs.push(bombNUmber);
     }
-    let squareperSide = Math.sqrt(numSquare);
-    console.log("celle per lato: ", squareperSide);
-    generaGriglia(numSquare, squareperSide);
   }
-  function generaGriglia(numSquare, squareperSide) {
-    console.log("numero di celle totali: ", numSquare);
-    const app = document.getElementById("app");
-    app.innerHTML = "";
-    let row = document.createElement("div");
-    row.setAttribute("class", "gridrow");
-    for (let i = 1; i <= numSquare; i++) {
-      const square = generaCella(i, squareperSide);
-      row.append(square);
-    }
-    app.append(row);
+}
+function generaGriglia(numSquare, squareperSide) {
+  console.log("numero di celle totali: ", numSquare);
+  const app = document.getElementById("app");
+  app.innerHTML = "";
+  let row = document.createElement("div");
+  row.setAttribute("class", "gridrow");
+  for (let i = 1; i <= numSquare; i++) {
+    const square = generaCella(i, squareperSide);
+    row.append(square);
   }
-  function generaCella(numSquare, squareperSide) {
-    let square = document.createElement("div");
-    square.setAttribute("class", "box");
-    square.style.width = `calc(100% / ${squareperSide})`;
-    square.style.height = `calc(100% / ${squareperSide})`;
-    square.classList.add("pointer");
-    square.innerHTML = `<span>${numSquare}</span>`;
-    square.addEventListener("click", coloraCella);
-    return square;
-  }
-  function coloraCella() {
-    //console.log(this);
+  app.append(row);
+}
+function generaCella(numSquare, squareperSide) {
+  let square = document.createElement("div");
+  square.setAttribute("class", "box");
+  square.style.width = `calc(100% / ${squareperSide})`;
+  square.style.height = `calc(100% / ${squareperSide})`;
+  square.classList.add("pointer");
+  square.innerHTML = `<span>${numSquare}</span>`;
+  square.addEventListener("click", coloraCella);
+  return square;
+}
+function coloraCella() {
+  //console.log(this.innerText);
+  let num = parseInt(this.innerText);
+  attempts++;
+  if (bombs.includes(num)) {
+    this.style.backgroundColor = "red";
+    this.innerHTML = `<img src="src/img/bomb.png">`;
+    gameOver();
+  } else {
     this.style.backgroundColor = "#6495ed";
-    this.classList.remove("pointer");
-    this.removeEventListener("click", coloraCella);
   }
-  //document.getElementById("level").addEventListener("change", setLevel);
-  document.getElementById("play").addEventListener("click", setLevel);
+  this.classList.remove("pointer");
+  this.removeEventListener("click", coloraCella);
+}
